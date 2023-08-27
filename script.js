c3fc6878556f78c7b36512b4e16ba8913f964d1c
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-26 20:54:33 -0500

    contact and project components started

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ const projectsBtn = document.getElementById('projects')
 const contactsBtn = document.getElementById('contacts')
 const wrapperEl = document.querySelector('.wrapper')
 const aboutEl = document.querySelector('.about')
+const projectEl = document.querySelector('.projects')
+const contactEl = document.querySelector('.contact')
 
 const aboutPageGenerate = () => {
     wrapperEl.style.display = "none";
     aboutEl.style.display = 'flex'
+    projectEl.style.display = 'none'
+}
+
+const projectsPageGenerate = () => {
+    wrapperEl.style.display = 'none';
+    aboutEl.style.display = 'none';
+    projectEl.style.display = 'flex'
+}
+
+const contactPageGenerate = () => {
+
 }
 
 aboutBtn.addEventListener('click', aboutPageGenerate)
+projectsBtn.addEventListener('click', projectsPageGenerate)
