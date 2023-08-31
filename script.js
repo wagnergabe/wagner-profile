b947ee90875db0e81bc5eeebc230d3f30d6094b6
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-30 22:52:29 -0500

    contact form working/css cleanup

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const aboutBtn = document.getElementById('about')
 const projectsBtn = document.getElementById('projects')
 const contactsBtn = document.getElementById('contacts')
+const submitBtn = document.getElementById('submitbtn')
 const wrapperEl = document.querySelector('.wrapper')
 const aboutEl = document.querySelector('.about')
 const projectEl = document.querySelector('.projects')
 const contactEl = document.querySelector('.contact')
+const thankYouEl = document.getElementById('thankYou')
 
 const aboutPageGenerate = () => {
     wrapperEl.style.display = "none";
-    aboutEl.style.display = 'flex'
+    aboutEl.style.display = 'block'
     projectEl.style.display = 'none'
     contactEl.style.display = 'none'
 }
@@ -27,6 +29,11 @@ const contactPageGenerate = () => {
     contactEl.style.display = 'flex'
 }
 
+const thankYou = () => {
+    thankYouEl.style.display = "block"
+}
+
 aboutBtn.addEventListener('click', aboutPageGenerate)
 projectsBtn.addEventListener('click', projectsPageGenerate)
 contactsBtn.addEventListener('click', contactPageGenerate)
+submitBtn.addEventListener('click', thankYou)
